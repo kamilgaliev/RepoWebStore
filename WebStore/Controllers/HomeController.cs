b07f6de3ae190e9a44cb9051b5cb04d7f80f90f2
kamilgaliev <kamil11122012@gmail.com>
2021-01-16 16:11:53 +0300

    Представления

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult SecondAction()
+        {
+            return Content("Second controller action");
+        }
+    }
+}
