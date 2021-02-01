6c1584a6ba0c76eca2d73b727b67f0c4978672bc
kamilgaliev <kamil11122012@gmail.com>
2021-02-01 20:57:02 +0300

    Визуальный компонент отоброжения брендов

---
+++ WebStore/ViewModels/BrandsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public record BrandsViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+    }
+}
