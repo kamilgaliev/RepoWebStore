6736b4fdae339fa4e05f5a1206f774c0226e0e50
kamilgaliev <kamil11122012@gmail.com>
2021-02-13 12:08:46 +0300

    Сервис корзины

--- WebStore/Infrastructure/Services/InSQL/SqlProductData.cs
+++ WebStore/Infrastructure/Services/InSQL/SqlProductData.cs
@@ -25,11 +25,18 @@ namespace WebStore.Infrastructure.Services.InSQL
         {
             IQueryable<Product> query = _db.Products;
 
-            if (Filter?.SectionId is { } section_id)
-                query = query.Where(product => product.SectionId == section_id);
+            if(Filter?.Ids?.Length > 0)
+            {
+                query = query.Where(product => Filter.Ids.Contains(product.Id));
+            }
+            else
+            {
+                if (Filter?.SectionId is { } section_id)
+                    query = query.Where(product => product.SectionId == section_id);
 
-            if (Filter?.BrandId is { } brand_id)
-                query = query.Where(product => product.BrandId == brand_id);
+                if (Filter?.BrandId is { } brand_id)
+                    query = query.Where(product => product.BrandId == brand_id);
+            }
 
             return query;
         }
