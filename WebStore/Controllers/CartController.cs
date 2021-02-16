e2ff7cefc159c26a8ea3d97600f513ca89f51502
kamilgaliev <kamil11122012@gmail.com>
2021-02-16 08:57:12 +0300

    Оформление заказа в контроллере корзины

--- WebStore/Controllers/CartController.cs
+++ WebStore/Controllers/CartController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -17,7 +16,10 @@ namespace WebStore.Controllers
         }
         public IActionResult Index()
         {
-            return View(_CartService.GetViewModel());
+            return View(new CartOrderViewModel
+            {
+                Cart = _CartService.GetViewModel()
+            });
         }
 
         public IActionResult Add(int id)
@@ -42,6 +44,33 @@ namespace WebStore.Controllers
         {
             _CartService.Clear();
             return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CheckOut(OrderViewModel OrderModel,[FromServices] IOrderService OrderService)
+        {
+            if (!ModelState.IsValid)
+                return View(nameof(Index), new CartOrderViewModel 
+                {
+                    Cart = _CartService.GetViewModel(),
+                    Order = OrderModel
+                });
+
+            var order = await OrderService.CreateOrder
+                (
+                    User.Identity!.Name,
+                    _CartService.GetViewModel(),
+                    OrderModel
+                );
+            _CartService.Clear();
+
+            return RedirectToAction(nameof(OrderConfirmed), new { order.Id });
+        }
+
+        public IActionResult OrderConfirmed(int id)
+        {
+            ViewBag.OrderId = id;
+            return View();
         }
     }
 }
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebStore.Conventions;
 using WebStore.DAL.Context;
 using WebStore.Data;
 using WebStore.Domain.Entities.Identity;
