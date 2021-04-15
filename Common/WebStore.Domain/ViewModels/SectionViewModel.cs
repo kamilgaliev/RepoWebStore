e5d9478c42f58b1d1e5f6fefa18b6c0807ab9f4b
kamilgaliev <kamil11122012@gmail.com>
2021-04-15 22:49:18 +0300

    Перенос моделей в Domain

---
+++ Common/WebStore.Domain/ViewModels/SectionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore.ViewModels
+{
+    public record SectionViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public int Order { get; init; }
+
+        public SectionViewModel Parent { get; init; }
+
+        public List<SectionViewModel> ChildSections { get; } = new ();
+
+        public int ProductsCount { get; set; }
+
+        public int TotalProductCount => ProductsCount + ChildSections.Sum(s => s.TotalProductCount);
+
+    }
+}
