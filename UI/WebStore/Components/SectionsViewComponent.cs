d2178d603ed184ea6d9487f0cad2d4b05bfd9d77
kamilgaliev <kamil11122012@gmail.com>
2021-05-03 19:30:17 +0300

    Выбор активной секции

--- UI/WebStore/Components/SectionsViewComponent.cs
+++ UI/WebStore/Components/SectionsViewComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using WebStore.Interfaces.Services;
 using WebStore.Domain.ViewModels;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
@@ -11,22 +12,37 @@ namespace WebStore.Components
         private readonly IProductData _ProductData;
 
         public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(string SectionId)
         {
+            var section_id = int.TryParse(SectionId, out var id) ? id : (int?) null;
+            var sections = GetSections(section_id, out var parent_section_id);
+
+            return View(new SelectableSectionsViewModel() 
+            { 
+                Sections = sections,
+                SectionId = section_id,
+                ParentSectionId = parent_section_id,
+            });
+        }
+
+        private IEnumerable<SectionViewModel> GetSections(int? SectionId, out int? ParentSectionId)
+        {
+            ParentSectionId = null;
+
             var sections = _ProductData.GetSections();
 
             var parent_sections = sections.Where(s => s.ParentId == null);
 
             var parent_sections_views = parent_sections
-                .Select(s => new SectionViewModel 
-                { 
+                .Select(s => new SectionViewModel
+                {
                     Id = s.Id,
                     Name = s.Name,
                     Order = s.Order,
                     ProductsCount = _ProductData.GetProducts(new Domain.ProductFilter { SectionId = s.Id }).Count()
                 }).ToList();
 
-            int OrderSortMethod(SectionViewModel a,SectionViewModel b) => Comparer<int>.Default.Compare(a.Order,b.Order);
+            int OrderSortMethod(SectionViewModel a, SectionViewModel b) => Comparer<int>.Default.Compare(a.Order, b.Order);
 
             foreach (var parent_section in parent_sections_views)
             {
@@ -34,6 +50,9 @@ namespace WebStore.Components
 
                 foreach (var child_sections in childs)
                 {
+                    if (child_sections.Id == SectionId)
+                        ParentSectionId = child_sections.ParentId;
+
                     parent_section.ChildSections.Add(new SectionViewModel
                     {
                         Id = child_sections.Id,
@@ -48,8 +67,7 @@ namespace WebStore.Components
             }
 
             parent_sections_views.Sort(OrderSortMethod);
-
-            return View(parent_sections_views);
+            return parent_sections_views;
         }
     }
 }
---
+++ UI/WebStore/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.ViewModels;
+
+namespace WebStore.ViewModels
+{
+    public class SelectableSectionsViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+        public int? SectionId { get; set; }
+        public int? ParentSectionId { get; set; }
+    }
+}
