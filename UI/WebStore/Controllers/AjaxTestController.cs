633a016082d86704c09a64f27e895d955a77922b
kamilgaliev <kamil11122012@gmail.com>
2021-05-11 23:32:33 +0300

    ненавязчивый AJAX

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetJSON(int? id, string msg, int Delay = 2000)
+        {
+            await Task.Delay(Delay);
+
+            return Json(new
+            {
+                Message = $"Response (id:{id ?? -1}): {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            }) ;
+        }
+
+        public async Task<IActionResult> GetHtml(int? id, string msg, int Delay = 2000)
+        {
+            await Task.Delay(Delay);
+
+            return PartialView("Partial/_DataView", new AjaxTestDataViewModel(id,msg, DateTime.Now));
+        }
+    }
+}
---
+++ UI/WebStore/ViewModels/AjaxTestDataViewModel.cs
@@ -0,0 +1,5 @@
+﻿using System;
+namespace WebStore.ViewModels
+{
+    public record AjaxTestDataViewModel(int? Id, string Message, DateTime ServerTime);
+}
